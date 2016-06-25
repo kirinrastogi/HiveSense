7a459aeb88de9506831452896b7a989b85dd8aeb
kirinrastogi <rastogikirin@gmail.com>
2016-06-25 13:18:16 -0400

    add view controllers

--- HiveSense/DataViewController.swift
+++ HiveSense/DataViewController.swift
@@ -6,16 +6,16 @@
 //  Copyright © 2016 Kirin Rastogi. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class DataViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,7 +27,9 @@ class DataViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-       // navigationItem.title = nil
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
+
+
 }
