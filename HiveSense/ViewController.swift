7ccfab1732a1b39312647b5dc5faeca474de31f7
kirinrastogi <rastogikirin@gmail.com>
2016-06-25 12:04:04 -0400

    add basic nav data and hives

--- HiveSense/ViewController.swift
+++ HiveSense/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(animated: Bool) {
+        navigationItem.title = "HiveSense"
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        navigationItem.title = nil
+    }
+    
 }
 
