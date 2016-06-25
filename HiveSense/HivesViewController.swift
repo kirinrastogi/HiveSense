b80cfc5be30e11d28bfae0702454544c15c87f3c
kirinrastogi <rastogikirin@gmail.com>
2016-06-25 12:57:20 -0400

    Add viewcontrollers

---
+++ HiveSense/HivesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HivesViewController.swift
+//  HiveSense
+//
+//  Created by Kirin Rastogi on 2016-06-25.
+//  Copyright © 2016 Kirin Rastogi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HivesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        navigationItem.title = "Hives"
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // navigationItem.title = nil
+    }
+    
+}
