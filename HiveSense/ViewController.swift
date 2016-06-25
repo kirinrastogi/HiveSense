00f9a19822562e5bde60badfe96c89ec1f6b91f5
kirinrastogi <rastogikirin@gmail.com>
2016-06-25 11:16:51 -0400

    Initial Commit

---
+++ HiveSense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiveSense
+//
+//  Created by Kirin Rastogi on 2016-06-25.
+//  Copyright © 2016 Kirin Rastogi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
